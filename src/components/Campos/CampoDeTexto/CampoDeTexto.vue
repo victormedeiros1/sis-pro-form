0ea2a3926612c76978c62aba9c8371efa881b65d
victormedeiros1 <josevictordev@gmail.com>
2024-06-24 22:50:41 -0300

    feat: componente CampoDeTexto

---
+++ src/components/Campos/CampoDeTexto/CampoDeTexto.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="campo-de-texto">
+    <label class="campo-de-texto__rotulo" :for="id">Título da pesquisa</label>
+    <InputText class="campo-de-texto__campo" :id="id" :label="rotulo" />
+  </div>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    id: string
+    rotulo: string
+  }
+
+  defineProps<Props>()
+</script>
+
+<style scoped lang="scss">
+  .campo-de-texto {
+    display: flex;
+    flex-direction: column;
+    gap: $g-8;
+
+    &__rotulo {
+      color: $dark;
+      font-weight: medium;
+      font-size: $fs-14;
+    }
+  }
+</style>
--- src/components/Formularios/FormCriarProjeto/FormCriarProjeto.vue
+++ src/components/Formularios/FormCriarProjeto/FormCriarProjeto.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="form">
-    <div class="criar-projeto"></div>
+  <div class="criar-projeto">
+    <div class="form">
+      <div class="form__cabecalho">
+        <h1>Dados gerais do projeto</h1>
+      </div>
+      <div class="form__corpo">
+        <div class="form__grupo">
+          <CampoDeTexto id="titulo-da-pesquisa" rotulo="Título da pesquisa" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import CampoDeTexto from '@/components/Campos/CampoDeTexto/CampoDeTexto.vue'
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  .criar-projeto {
+    .form {
+      &__cabecalho {
+        margin-bottom: $m-16;
+      }
+    }
+  }
+</style>
