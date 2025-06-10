653f54998461b4ec4f74ee40f2ea9a01aec9cef8
victormedeiros1 <josevictordev@gmail.com>
2025-06-09 23:04:17 -0300

    feat: componente CampoDeCPF

---
+++ src/components/Campos/CampoDeCPF/CampoDeCPF.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="campo-de-cpf">
+		<label class="campo-de-cpf__rotulo rotulo" :for="id">{{ rotulo }}</label>
+		<InputText
+			class="campo-de-cpf__campo campo"
+			:id="id"
+			:label="rotulo"
+			:placeholder="textoAuxiliar"
+			@update:model-value="emitirCPF"
+			v-model="cpf"
+			size="small"
+		/>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { nextTick, ref } from 'vue'
+
+interface Props {
+	id: string
+	rotulo: string
+	textoAuxiliar?: string
+}
+
+defineProps<Props>()
+
+const emitir = defineEmits<{
+	(evento: 'update:cpf', cpf: string): void
+}>()
+
+const cpf = ref('')
+
+const emitirCPF = (): void => {
+	nextTick(() => {
+		emitir('update:cpf', cpf.value)
+	})
+}
+</script>
+
+<style scoped lang="scss">
+.campo-de-cpf {
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	gap: var(--g-8);
+}
+</style>
